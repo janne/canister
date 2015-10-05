0fcdfc40eced5800d4ca73067a98a8733a27bb15
Jan Andersson <jan.andersson@gmail.com>
2015-10-05 13:23:25 +0200

    Include web3 dependency

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
+require('web3');
 
 var App = React.createClass({
   render: function() {
+    var version = web3.version.api;
     return (
-      <div>Hello, world!</div>
+      <div>Web3 version: {version}</div>
     );
   }
 });
