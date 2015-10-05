68fa81309e9f927d47b0fb2353e2300d0f394253
Jan Andersson <jan.andersson@gmail.com>
2015-10-05 16:56:18 +0200

    Add balance and coinbase state to Account component

--- src/account.jsx
+++ src/account.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
+var web3 = require('web3');
 
 var Account = React.createClass({
+  getInitialState: function() {
+    return {
+      isCoinbase: (this.props.address == web3.eth.coinbase),
+      balance: web3.eth.getBalance(this.props.address)
+    }
+  },
   render: function() {
-    var address = this.props.address;
-    return (<div>{address}</div>);
+    var addr = this.state.isCoinbase ? <b>{this.props.address}</b> : <span>{this.props.address}</span>
+    var balance = this.state.balance;
+    return (<div>{addr}: {balance}</div>)
   }
 });
 
